543590fc316f5a064b3d1038fdc01ba45876fa1d
Nathan Dunn <me@ndunn.uk>
2019-02-18 19:56:44 +0000

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Button label="+" event="increment" @increment="incrementCount" />
+    <Button label="-" event="decrement" @decrement="decrementCount" />
+
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue'
+
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      count: 0,
+    }
+  },
+  components: {
+    Button
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    },
+    decrementCount() {
+      this.count--;
+    }    
+  }
+}
+</script>
+
+<style>
+* {
+  font-size: 1.5rem;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="buttonClicked">{{ label }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String      
+    },
+    event: {
+      type: String,
+        validator: (val) => ['increment', 'decrement'].includes(val)
+    }
+  },
+  methods: {
+    buttonClicked() {
+      this.$emit(this.event);
+    }
+  }
+}
+</script>
+
+<style>
+button {
+  width: 3rem;
+}
+</style>
